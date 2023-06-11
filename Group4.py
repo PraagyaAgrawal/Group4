1087566ff55928371eaf34b5896c0994fd50808e
PraagyaAgrawal <123302560+PraagyaAgrawal@users.noreply.github.com>
2023-06-11 14:57:16 +0800

    Update Group4.py

--- Group4.py
+++ Group4.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import numpy as np
 
+st.set_page_config(page_title = "ACE Toiletz", page_icon = ":poop:", layout = "centered", initial_sidebar_state = "auto")
+
 st.title("ACE Toiletz")
 
 st.sidebar.title("Input features")
